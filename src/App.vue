7c6d5917f5df3e85bdd97caaf1404c145d1f148f
Adenilsson <adenilson@fairtek.com.br>
2021-10-07 14:22:48 -0300

    Life Cycles hooks

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,24 @@
 <template>
-<PrimeiroComponente />
+<div>
+  <PrimeiroComponente />
+  <p>O dador de app é:{{testando}}</p>
+  <LifeCycle/>
+</div>
 </template>
 <script >
 import PrimeiroComponente from './components/PrimeiroComponente.vue'
+import LifeCycle from './components/LifeCycles.vue' 
   export default{
     name:'App',
     components:{
-      PrimeiroComponente
+      PrimeiroComponente,
+      LifeCycle
+    },
+    data(){
+      return {
+         testando: "teste de componente dentro de AppVue"
+      }
+     
     }
   }
 </script>
--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -1,8 +1,20 @@
 <template>
-<h1> Olá Vue! </h1>
+<div>
+    <h1> Olá Vue! </h1>
+    <p>Meu nome é {{nome}}.</p>
+    <p> Profissao:{{profissao}}.</p>
+</div>
 </template>
+
+
 <script>
 export default {
-    name: 'PrimeiroComponente'
+    name: 'PrimeiroComponente',
+    data(){
+        return{
+            nome: "Adenilson",
+            profissao:"Programador"
+        }
+    }
 }
 </script>
