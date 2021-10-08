8fc60b3cc8f67de56ad13705e8e389bc5dd7477e
Adenilsson <adenilson@fairtek.com.br>
2021-10-08 15:56:45 -0300

    Multiplos eventos

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,14 +1,16 @@
 <template>
 <p>TESTE DE FORMULARIO</p>
-<form action="">
+<p v-if="form_enviado">Nome digitado é {{name}}</p>
+<p v-if="form_enviado">Email informado {{email}}</p>
+<form action="" @submit="enviarFormulario($event)">
 <div>
-<InputText />
+<input type="text" v-model="name">
 </div>
 <div>
-<InputText />
+<input type="text" v-model="email">
 </div>
 <div>
-<Enviar />
+<input type="submit" value="Enviar" @click="dados_enviados">
 </div>,
 </form>
 </template>
@@ -17,10 +19,34 @@ import InputText from './forms/InputText.vue'
 import Enviar from './forms/Enviar.vue'
 export default{
     name:'Formulario',
+    data(){
+        return{
+            name: '',
+            email:'',
+            enviar: false,
+            form_enviado: false
+        }
+    },
     components:{
         Enviar,
         InputText
         
+    },
+    methods:{
+        enviarFormulario(e){
+            e.preventDefault();
+
+            const name=  this.name;
+            const email =  this.email;
+            console.log('enviarFormulario',name, email);
+        },
+        dados_enviados(){
+            if(this.enviar){
+                this.form_enviado = false;
+            }else{
+                this.form_enviado = true;
+            }
+        }
     }
 }
 </script>
--- src/components/PrimeiroComponente.vue
+++ src/components/PrimeiroComponente.vue
@@ -3,18 +3,25 @@
     <h1> Olá Vue! </h1>
     <p>Meu nome é {{nome}}.</p>
     <p> Profissao:{{profissao}}.</p>
+    <MultiplosEvent />
 </div>
 </template>
 
 
 <script>
+import MultiplosEvent from './MultiplosEventos.vue'
 export default {
+    
     name: 'PrimeiroComponente',
     data(){
         return{
             nome: "Adenilson",
             profissao:"Programador"
         }
+    },
+    components:{
+        MultiplosEvent
+
     }
 }
 </script>
