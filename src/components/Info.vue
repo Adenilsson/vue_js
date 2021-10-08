cd6266418a197c6302402af4c4f9c7f589b3d61b
Adenilsson <adenilson@fairtek.com.br>
2021-10-08 14:06:44 -0300

    v-form arraylist e objetos

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -3,15 +3,14 @@
     <Picture />
     <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
     <p v-else>Não estou trabalhando no momento</p>
-    <p>Utilizo as seguintes tecnologias: </p>
-    <ul>
-        <li>Javascript</li>
-        <li>PHP</li>
-        <li>Python</li>
-        <li>Java</li>
-        <li>C++</li>
-
+    <p>Utilizo as seguintes tecnologias para backend </p>
+    <ul v-for="technology in backend_tecnology">
+        <li>{{technology}}</li>
     </ul>
+    <p>Tecnologias usadas no Frontend: </p>
+    <lu v-for="linguagens in frontend_tecnology">
+        <li>{{linguagens.tecnology}}</li>
+    </lu>
     <div>
         <button @click="showEmail">{{texbooton}}</button>
     </div>
@@ -30,7 +29,14 @@ export default{
             mostrar_email: false,
             email: 'adenilson@fairtek.com.br',
             meu_link:'https://google.com',
-            texbooton:'Mostrar e-mail'
+            texbooton:'Mostrar e-mail',
+            backend_tecnology:['JavaScript','PHP','Python','java','C++'],
+            frontend_tecnology:[
+                {id:1,tecnology: 'HTML5'},
+                {id:2,tecnology: 'CSS'},
+                {id:3,tecnology:'vue'}
+
+                ]
         }
     },
     components:{
